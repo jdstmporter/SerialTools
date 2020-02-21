44beb8061678e07f2db9ad4216f148b80852edd5
julian porter <julian@porternet.org.uk>
2020-02-21 21:28:51 +0000

    Initial Commit

---
+++ Serial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Serial
+//
+//  Created by Julian Porter on 21/02/2020.
+//  Copyright © 2020 JP Embedded Solutions. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
